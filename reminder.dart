2e6a99c6701a05e84219f9c932cdc1643949974a
Wichananm65 <wichananm65@u.ac.th>
2024-11-18 15:49:54 +0700

    1

---
+++ reminder.dart
@@ -0,0 +1,11 @@
+void main(){
+  int dividend = 10;
+  int divisor = 3;
+
+  double quotient = dividend / divisor;
+  int remainder = dividend % divisor;
+
+  print("HHH");
+  print('quotient = $quotient');
+  print('remainder = $remainder');
+}
