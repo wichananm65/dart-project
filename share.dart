c2f825994e2e1afbb07ddc1adaf4e557ce122e2a
Wichanan <wichananm65@nu.ac.th>
2024-11-25 13:13:59 +0700

    2

---
+++ share.dart
@@ -0,0 +1,7 @@
+void main(){
+  int peopleNum = 5;
+  int totalPrice = 470;
+
+  double price = totalPrice / peopleNum; 
+  print('$result');
+}
--- splitBill.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  int peopleNum = 5;
-  int billAmount = 470;
-
-  double result = billAmount / peopleNum; 
-  print('$result');
-}
